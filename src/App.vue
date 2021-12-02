39f7040bf3b73f7ff40ba1f6692050a012374dd9
helsenberg <antonovdiman71@gmail.com>
2021-12-02 20:32:19 +0300

    Added colors

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,35 @@
 }
 
 :root {
-  --orange-50: #FAD7C8;
-
-
-  --gray-50: #CFCFD8;
-
-  --blue-50: #C5C3EC;
-
-  --green-50: #DCEBB8;
+  /* Primary Colors */
+  --primary_orange: #FA4616 ;
+  --primary_blue: #009FDA ;
+  --primary_linear: #FA4616 ;
+  /* Text */
+  --body_text: #30353B ;
+  --body_text_blue: #0087B4 ;
+  --warning_text: #FF1A1A ;
+  --neutral_text: #979A9D ;
+  /* Description text */
+  --description_text_primary: #747F8B ;
+  --description_text_secondary: #E3E5E8 ;
+  /* Alert text */
+  --alert_text_green: #00B45A ;
+  --alert_text_yellow: #E19600 ;
+  /* Alerts */
+  --success: #E1FFE6;
+  --important_notifications: #FFFADC;
+  --error_warning: #FFE6E6;
+  --information: #E6F5FA;
+  /* Buttons */
+  --buttons_default: #FA4616 ;
+  --buttnos_hover: #F03200 ;
+  --buttons_active: #F03200 ;
+  /* Banners */
+  --banners_light_yellow: #FEEFBF ;
+  --banners_light_emerald: #BFECD6 ;
+  --banners_light_purple: #C7BBD7 ;
+  --banners_light_green: #DDECBF ;
 }
-
 
 </style>
